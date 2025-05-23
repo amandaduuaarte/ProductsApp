4f635637474876fdd51c70c652fb908eb6de10f3
Amanda Duarte <amandaduuaarte@gmail.com>
2025-05-22 22:02:12 -0300

    chore: config EsLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
-module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+    module.exports = {
+      root: true,
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-native/recommended',
+        'airbnb',
+        'prettier',
+      ],
+      plugins: ['react', 'react-native', 'prettier'],
+      rules: {
+        'prettier/prettier': 'error',
+        'react/tsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+        'react/prop-types': 'off',
+        'no-use-before-define': 'off',
+        'react-native/no-inline-styles': 'off',
+      },
+      env: {
+        'react-native/react-native': true,
+      },
+    };
