dfc20ad6d5eb3985fc8fd87d1503d354329fc8c8
Amanda Duarte <amandaduuaarte@gmail.com>
2025-05-22 22:19:14 -0300

    fix: adjusting lint to fix on save

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,38 @@
-    module.exports = {
-      root: true,
-      extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react-native/recommended',
-        'airbnb',
-        'prettier',
-      ],
-      plugins: ['react', 'react-native', 'prettier'],
-      rules: {
-        'prettier/prettier': 'error',
-        'react/tsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
-        'react/prop-types': 'off',
-        'no-use-before-define': 'off',
-        'react-native/no-inline-styles': 'off',
-      },
-      env: {
-        'react-native/react-native': true,
-      },
-    };
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    'react-native',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'react/prop-types': 'off',
+    'react-native/no-inline-styles': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error']
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
