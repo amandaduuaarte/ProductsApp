828c91b42e5de9ec0a726adae968272f5a722dfd
Amanda Duarte <amandaduuaarte@gmail.com>
2025-05-22 22:07:33 -0300

    feat: create folder structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@presentation': './src/presentation',
+          '@domain': './src/domain',
+          '@infra': './src/infra',
+          '@shared': './src/shared'
+        }
+      }
+    ]
+  ]
 };
