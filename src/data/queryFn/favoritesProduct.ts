10ec2d6a762541bb4fce3ccd254094ac3c93d254
Amanda Duarte <amandaduuaarte@gmail.com>
2025-05-26 13:32:05 -0300

    fix: adjusting fav state

--- src/data/queryFn/favoritesProduct.ts
+++ src/data/queryFn/favoritesProduct.ts
@@ -1,7 +1,10 @@
 import {storage} from '@lib/storage/mmkv';
+import {useState} from 'react';
 
 const STORAGE_KEY = 'favorites-products';
 export const favoritesProductFn = () => {
+  const [favorites, setFavorites] = useState<boolean>();
+
   const getProductsId = (): number[] => {
     const data = storage.getString(STORAGE_KEY);
     return data ? JSON.parse(data) : [];
@@ -10,14 +13,16 @@ export const favoritesProductFn = () => {
   const saveProductsId = (id: number) => {
     const current = getProductsId();
     const updated = Array.from(new Set([...current, id]));
+    setFavorites(prev => !prev);
     storage.set(STORAGE_KEY, JSON.stringify(updated));
   };
 
   const removeProduct = (id: number) => {
     const current = getProductsId();
     const updated = current.filter(item => item !== id);
+    setFavorites(prev => !prev);
     storage.set(STORAGE_KEY, JSON.stringify(updated));
   };
 
-  return {saveProductsId, removeProduct, getProductsId};
+  return {saveProductsId, removeProduct, getProductsId, favorites};
 };
--- src/domain/useCases/useFavoritesProductsUseCase.ts
+++ src/domain/useCases/useFavoritesProductsUseCase.ts
@@ -1,11 +1,12 @@
 import {favoritesProductFn} from '@data/queryFn/favoritesProduct';
 
 export const useFavoritesProductsUseCase = () => {
-  const {saveProductsId, removeProduct, getProductsId} = favoritesProductFn();
+  const {saveProductsId, removeProduct, getProductsId, favorites} =
+    favoritesProductFn();
 
   const isProductFav = (productId: number) => {
     const current = getProductsId();
-    return current.includes(productId);
+    return current.includes(productId) || favorites;
   };
 
   return {saveProductsId, removeProduct, isProductFav};
