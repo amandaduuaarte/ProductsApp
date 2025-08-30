db27e453a374b64443ff4ba8fcda9536512bb928
Amanda Duarte <amandaduuaarte@gmail.com>
2025-08-30 14:51:15 -0300

    feat: adjusting queryFn name's

--- src/data/queryFn/getProductDetails.ts
+++ src/data/queryFn/getProductDetails.ts
@@ -3,7 +3,7 @@ import {productDetailsSchema, TProduct} from '@data/schema/products.schema';
 import {httpClient} from '@lib/httpClient/axios';
 
 const {api} = httpClient();
-export const getProductsDetailsFn = () => {
+export const useGetProductsDetailsFn = () => {
   const get = async (id: number): Promise<TProduct | undefined> => {
     try {
       const response = await api.get(`/products/${id}`);
--- src/data/services/getProductsCategoriesService.ts
+++ src/data/services/getProductsCategoriesService.ts
@@ -1,12 +1,13 @@
-import {getProductsCategoriesFn} from '@data/queryFn/getProductsCategories';
+import {useGetProductsCategoriesFn} from '@data/queryFn/getProductsCategories';
 import {useQuery} from '@tanstack/react-query';
 
 export const PRODUCTS_CATEGORIES_QUERY_KEY = 'PRODUCTS_CATEGORIES_KEY';
 
-const {get} = getProductsCategoriesFn();
+export const useGetProductsCategoriesService = ({limit}: {limit?: number}) => {
+  const {get} = useGetProductsCategoriesFn();
 
-export const useGetProductsCategoriesService = ({limit}: {limit?: number}) =>
-  useQuery({
+  return useQuery({
     queryFn: () => get(limit),
     queryKey: [PRODUCTS_CATEGORIES_QUERY_KEY, limit],
   });
+};
